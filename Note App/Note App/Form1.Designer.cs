26ae073bf6e072cf8f2bf76aecfe7f97d2024e07
Jacob Cooper <jacob.cooper448@gmail.com>
2021-02-04 09:14:24 +0000

    search works now

--- Note App/Note App/Form1.Designer.cs
+++ Note App/Note App/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Note_App
 {
-    partial class Form1
+    partial class TheIndex
     {
         /// <summary>
         /// Required designer variable.
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -38,14 +39,18 @@
             this.newBtn = new System.Windows.Forms.Button();
             this.searchBtn = new System.Windows.Forms.Button();
             this.solutionList = new System.Windows.Forms.DataGridView();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.solutionMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.solutionList)).BeginInit();
+            this.solutionMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(13, 27);
+            this.label3.Location = new System.Drawing.Point(13, 15);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(27, 13);
             this.label3.TabIndex = 0;
@@ -55,7 +60,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
-            this.label4.Location = new System.Drawing.Point(12, 62);
+            this.label4.Location = new System.Drawing.Point(12, 41);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(60, 13);
             this.label4.TabIndex = 1;
@@ -65,7 +70,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
-            this.label5.Location = new System.Drawing.Point(13, 197);
+            this.label5.Location = new System.Drawing.Point(13, 175);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(45, 13);
             this.label5.TabIndex = 2;
@@ -73,25 +78,25 @@
             // 
             // titleTxt
             // 
-            this.titleTxt.Location = new System.Drawing.Point(80, 24);
+            this.titleTxt.Location = new System.Drawing.Point(79, 12);
             this.titleTxt.Name = "titleTxt";
             this.titleTxt.Size = new System.Drawing.Size(294, 20);
             this.titleTxt.TabIndex = 3;
             // 
             // desTxt
             // 
-            this.desTxt.Location = new System.Drawing.Point(79, 59);
+            this.desTxt.Location = new System.Drawing.Point(79, 38);
             this.desTxt.Multiline = true;
             this.desTxt.Name = "desTxt";
-            this.desTxt.Size = new System.Drawing.Size(396, 117);
+            this.desTxt.Size = new System.Drawing.Size(396, 128);
             this.desTxt.TabIndex = 4;
             // 
             // solTxt
             // 
-            this.solTxt.Location = new System.Drawing.Point(79, 194);
+            this.solTxt.Location = new System.Drawing.Point(79, 172);
             this.solTxt.Multiline = true;
             this.solTxt.Name = "solTxt";
-            this.solTxt.Size = new System.Drawing.Size(396, 207);
+            this.solTxt.Size = new System.Drawing.Size(396, 229);
             this.solTxt.TabIndex = 5;
             // 
             // saveBtn
@@ -123,19 +128,52 @@
             this.searchBtn.TabIndex = 9;
             this.searchBtn.Text = "Search";
             this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
             // 
             // solutionList
             // 
+            this.solutionList.AllowUserToAddRows = false;
+            this.solutionList.AllowUserToDeleteRows = false;
+            this.solutionList.AllowUserToResizeColumns = false;
+            this.solutionList.AllowUserToResizeRows = false;
             this.solutionList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.solutionList.Location = new System.Drawing.Point(502, 24);
+            this.solutionList.Location = new System.Drawing.Point(502, 15);
             this.solutionList.Name = "solutionList";
-            this.solutionList.Size = new System.Drawing.Size(240, 377);
+            this.solutionList.ReadOnly = true;
+            this.solutionList.RowHeadersVisible = false;
+            this.solutionList.Size = new System.Drawing.Size(240, 386);
             this.solutionList.TabIndex = 6;
             this.solutionList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.solutionList_CellClick);
+            this.solutionList.MouseDown += new System.Windows.Forms.MouseEventHandler(this.solutionList_MouseDown);
             // 
-            // Form1
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(502, 406);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 10;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // solutionMenuStrip
+            // 
+            this.solutionMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem});
+            this.solutionMenuStrip.Name = "solutionMenuStrip";
+            this.solutionMenuStrip.Size = new System.Drawing.Size(95, 26);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // TheIndex
             // 
             this.ClientSize = new System.Drawing.Size(754, 479);
+            this.Controls.Add(this.deleteBtn);
             this.Controls.Add(this.searchBtn);
             this.Controls.Add(this.newBtn);
             this.Controls.Add(this.saveBtn);
@@ -146,10 +184,13 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Name = "Form1";
+            this.Name = "TheIndex";
+            this.Text = "The Index";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.solutionList)).EndInit();
+            this.solutionMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,15 +198,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label2;
+        
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
@@ -176,6 +209,9 @@
         private System.Windows.Forms.Button saveBtn;
         private System.Windows.Forms.Button newBtn;
         private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.ContextMenuStrip solutionMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
     }
 }
 
--- Note App/Note App/Form1.cs
+++ Note App/Note App/Form1.cs
@@ -11,10 +11,12 @@ using System.IO;
 
 namespace Note_App
 {
-    public partial class Form1 : Form
+    public partial class TheIndex : Form
     {
-        DataTable table;
-        public Form1()
+        float firstWidth;
+        float firstHeight;
+        public DataTable table;
+        public TheIndex()
         {
             InitializeComponent();
         }
@@ -35,10 +37,21 @@ namespace Note_App
                 table.Columns.Add("Solution", typeof(String));
             }
           
-
             solutionList.DataSource = table;
-        }
 
+            solutionList.Columns["Description"].Visible = false;
+            solutionList.Columns["Solution"].Visible = false;
+
+            solutionList.Columns["Title"].Width = solutionList.Size.Width -3;
+
+            firstWidth = this.Size.Width;
+            firstHeight = this.Size.Height;
+            solutionList.ContextMenuStrip = solutionMenuStrip;
+            
+
+        }
+        
+       
         private void newBtn_Click(object sender, EventArgs e)
         {
             titleTxt.Clear();
@@ -48,7 +61,31 @@ namespace Note_App
 
         private void saveBtn_Click(object sender, EventArgs e)
         {
-            table.Rows.Add(titleTxt.Text, desTxt.Text, solTxt.Text);
+            
+            bool contains = false;
+            foreach (DataGridViewRow row in solutionList.Rows)
+            {
+                
+                Console.WriteLine(row.Cells[0].Value);
+                if ((string)row.Cells[0].Value == titleTxt.Text)
+                {
+                    warningPopup warn = new warningPopup();
+                    warn.ShowDialog();
+                    if(warn.Replace == true)
+                    {
+                        row.Cells[1].Value = desTxt.Text;
+                        row.Cells[2].Value = solTxt.Text;
+                    }
+                    contains = true;
+                    break;
+                }
+               
+            }
+            if(contains == false)
+            {
+                table.Rows.Add(titleTxt.Text, desTxt.Text, solTxt.Text);
+            }
+
             titleTxt.Clear();
             desTxt.Clear();
             solTxt.Clear();
@@ -58,10 +95,12 @@ namespace Note_App
         {
            try 
             {
-                titleTxt.Text = solutionList.Rows[e.RowIndex].Cells[0].Value.ToString();
-                desTxt.Text = solutionList.Rows[e.RowIndex].Cells[1].Value.ToString();
-                solTxt.Text = solutionList.Rows[e.RowIndex].Cells[2].Value.ToString();
-               
+                //titleTxt.Text = solutionList.Rows[e.RowIndex].Cells[0].Value.ToString();
+                // desTxt.Text = solutionList.Rows[e.RowIndex].Cells[1].Value.ToString();
+                // solTxt.Text = solutionList.Rows[e.RowIndex].Cells[2].Value.ToString();
+                SolutionInfo solu = new SolutionInfo(solutionList.Rows[e.RowIndex].Cells[0].Value.ToString(), solutionList.Rows[e.RowIndex].Cells[1].Value.ToString(), solutionList.Rows[e.RowIndex].Cells[2].Value.ToString());
+                solu.ShowDialog();
+
             }
             catch(ArgumentOutOfRangeException a)
             {
@@ -73,6 +112,67 @@ namespace Note_App
         {
             
             table.WriteXml("saved.xml", XmlWriteMode.WriteSchema);
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = solutionList.CurrentCell.RowIndex;
+
+                table.Rows[index].Delete();
+            }
+            catch (NullReferenceException a )
+            {
+                MessageBox.Show("There is no solution to delete");
+            }
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            float size1 = this.Size.Width / firstWidth;
+            float size2 = this.Size.Height / firstHeight;
+
+            SizeF scale = new SizeF(size1, size2);
+            firstWidth = this.Size.Width;
+            firstHeight = this.Size.Height;
+
+            foreach (Control control in this.Controls)
+            {
+
+                control.Font = new Font(control.Font.FontFamily, control.Font.Size * ((size1 + size2) / 2));
+
+                control.Scale(scale);
+                solutionList.Columns["Title"].Width = solutionList.Size.Width - 3;
+
+            }
+
+
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            Search search = new Search(table, this);
+            search.Show();
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            int index = solutionList.Rows.GetFirstRow(DataGridViewElementStates.Selected);
+            titleTxt.Text = solutionList.Rows[index].Cells[0].Value.ToString();
+            desTxt.Text = solutionList.Rows[index].Cells[1].Value.ToString();
+            solTxt.Text = solutionList.Rows[index].Cells[2].Value.ToString();
+        }
+
+        private void solutionList_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                solutionList.ClearSelection();
+                solutionList.Rows[solutionList.HitTest(e.X, e.Y).RowIndex].Selected = true;
+            }
         }
     }
 }
